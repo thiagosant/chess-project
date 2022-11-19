41aed1f87d604ab4aa5c88a864c7ab8495a8b863
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2022-11-19 16:41:09 -0300

    Beginning the project

---
+++ src/main/java/com/sansoft/chessproject/ChessProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sansoft.chessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessProjectApplication.class, args);
+    }
+
+}
